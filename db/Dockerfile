7e393d06c1967354d6dad06e965a26303d0b0ba9
luuucasmoreira <luuucasmoreira1994@gmail.com>
2024-08-30 11:10:22 -0300

    feat: Inicio do projeto com erro no nginx na leitura do index.php

---
+++ db/Dockerfile
@@ -0,0 +1,3 @@
+FROM mariadb:latest
+
+COPY init.sql /docker-entrypoint-initdb.d/
---
+++ php/index.php
@@ -0,0 +1,10 @@
+<?php
+
+// Mostra todas as informações, equivalente ao padrão INFO_ALL
+phpinfo();
+
+// Mostra apenas informações sobre módulos.
+// phpinfo(8) gera resultados idênticos.
+phpinfo(INFO_MODULES);
+
+?>
